989f2eb6eda00dfa3b0d8193316705b9317b03b4
Owner <alsalamcse>
2019-09-19 14:07:22 +0300

    gbukjgjyg

--- app/src/main/java/com/yassen/mossab/mossabtaskmanger/MossabTaskManger.java
+++ app/src/main/java/com/yassen/mossab/mossabtaskmanger/MossabTaskManger.java
@@ -1,13 +1,34 @@
 package com.yassen.mossab.mossabtaskmanger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MossabTaskManger extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mossab_task_manger);
+
+        Thread th=new Thread()
+        {
+            @Override
+        public void run()
+        {
+            try {
+                sleep(3*1000);
+                Intent i=new Intent(getApplication(),SignIn.class);
+                startActivity(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        };
+
+        th.start();
+
     }
 }
