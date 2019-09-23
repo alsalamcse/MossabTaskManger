44b0cb6c1964abab1104aa0d1c7998b734646ccd
Owner <alsalamcse>
2019-09-23 09:42:21 +0300

    gbukjgjyg

--- app/src/main/java/com/yassen/mossab/mossabtaskmanger/SignIn.java
+++ app/src/main/java/com/yassen/mossab/mossabtaskmanger/SignIn.java
@@ -2,27 +2,82 @@ package com.yassen.mossab.mossabtaskmanger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignIn extends AppCompatActivity {
     private ImageView imageView;
-    private EditText edtEmail,edtPassword;
-    private Button btnSignIn,btnSignUp;
+    private EditText edtEmail, edtPassword;
+    private Button btnSignIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        imageView=(ImageView)(findViewById(R.id.imageView));
-        edtEmail=(EditText)(findViewById(R.id.edtEmail));
-        edtPassword=(EditText)(findViewById(R.id.edtPassword));
-        btnSignIn=(Button)(findViewById(R.id.btnSignIn));
-        btnSignUp=(Button)(findViewById(R.id.btnSignUp));
+        imageView = (ImageView) (findViewById(R.id.imageView));
+        edtEmail = (EditText) (findViewById(R.id.edtEmail));
+        edtPassword = (EditText) (findViewById(R.id.edtPassword));
+        btnSignIn = (Button) (findViewById(R.id.btnSignIn));
+        btnSignUp = (Button) (findViewById(R.id.btnSignUp));
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataHandler();
+            }
+        });
 
     }
 
+    private void dataHandler() {
+        String email = edtEmail.getText().toString();
+        String password = edtPassword.getText().toString();
+        boolean isok = true;
 
+//        if (email.length() < 4)
+//        {
+//            edtEmail.setError("Email lenght error");
+//            isok = false;
+//        }
+
+//        if (email.indexOf("@") < 0 || email.indexOf(".") < 0) {
+//            edtEmail.setError("email wrong format");
+//            isok = false;
+//        }
+
+        if (isValidEmailAddress(email)==false)
+        {
+            edtEmail.setError("Invalid Email");
+            isok=false;
+        }
+
+        if (password.length() < 8) {
+            edtPassword.setError("password lenght error");
+            isok = false;
+        }
+        if (isok)
+        {
+
+        }
+
+    }
+
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+
+    }
+
+    private void signIn(String email, String password)
+    {
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email,password);
+
+    }
 }
